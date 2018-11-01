a79b87f7057b12b210ced0f2ef21d49b403b3daf
yu.shichao <yu.shichao@liteshare.cn>
2018-11-01 14:22:00 +0800

    "New algorithm"

--- app/src/main/java/com/example/yushichao/bufferdetector/AccControl.java
+++ app/src/main/java/com/example/yushichao/bufferdetector/AccControl.java
@@ -26,7 +26,55 @@ public class AccControl {
         count=0;
     }
 
-    public void refreshAcc(float[] values){
+    public void refreshAcc(float[] values) {
+        float acc = (float) Math.sqrt(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]);
+
+        Log.e("Acc", "" + acc);
+        Accs[count] = acc;
+
+        float[] data = new float[Accs.length];
+        for (int i = 0, j = count; i < Accs.length; i++, j--) {
+            if (j < 0) {
+                j = Accs.length - 1;
+            }
+            data[i] = Accs[j];
+        }
+
+        data=Utils.smoothFilter(data,5);
+
+        int r=100;
+        float[] ave=new float[data.length];
+        float[] var=new float[data.length];
+
+        float sum=0;
+        for (int i=0;i<data.length;i++){
+            if (i<2*r){
+                 sum+=data[i];
+            }else{
+                sum+=data[i];
+                sum-=data[i-2*r];
+
+                ave[i-r]=sum/(2*r);
+                float v=0;
+                for (int j=i-2*r+1;j<=i;j++){
+                    v+=(data[j]-ave[i-r])*(data[j]-ave[i-r]);
+                }
+                var[i-r]=v/(2*r);
+            }
+        }
+
+        float[] sign=new float[data.length];
+        for (int i=r;i<data.length-r;i++){
+            if (data[i]>1.5&&data[i]>2*ave[i]&&var[i]*3>ave[i]){
+                sign[i]=1;
+            }
+        }
+
+        callback.Drew(data,sign);
+        count=(count+1)%Accs.length;
+    }
+
+    public void refreshAcc2(float[] values){
         float acc=(float)Math.sqrt(values[0]*values[0]+values[1]*values[1]+values[2]*values[2]);
 
         Log.e("Acc",""+acc);
